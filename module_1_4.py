d066962e606443224a9cee0f90788596354d0fe1
Zerber7 <zerber_@mail.ru>
2024-09-22 20:13:28 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# my_string = input('У лукоморья дуб зелёный')
+my_string = 'У лукоморья дуб зелёный'
+print(my_string)
+my_string = 'У лукоморья дуб зелёный'
+len('У лукоморья дуб зелёный')
+str = 'У лукоморья дуб зелёный'
+print(len(str))
+print('У лукоморья дуб зелёный'.upper())
+print('У лукоморья дуб зелёный'.upper().lower())
+print(my_string. replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
